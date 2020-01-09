e0cb93e1c2afdeaf9d94e4c630d35302721c129e
Kheva <khevamann@gmail.com>
2020-01-09 15:44:28 -0800

    Refactor for Github pages

--- firebase.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\firebase-tools\bin\firebase" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\firebase-tools\bin\firebase" %*
-)
